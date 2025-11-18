c78af476a205df4a1f676f227b40eb2b118aee6a
Robin Gusain <gusainrobin37@gmail.com>
2025-11-18 12:23:28 +0530

    Updated GUI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,10 @@ import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
   const [roomId, setRoomId] = useState("");
-  const[username, setUsername] = useState("");
+  const [username, setUsername] = useState("");
   const createNewRoom = (e) => {
     e.preventDefault();
     const id = uuidV4();
@@ -17,22 +17,23 @@ const Home = () => {
   };
 
   const joinRoom = () => {
-    if(!roomId || !username){
+    if (!roomId || !username) {
       toast.error("Room ID & username is required");
       return;
     }
     // Redirect
-    navigate(`/editor/${roomId}`,{state:{
-      username,
-    }});  
-  
+    navigate(`/editor/${roomId}`, {
+      state: {
+        username,
+      },
+    });
   };
 
-  const handleInputEnter=(e)=>{
-    if(e.code==="Enter"){
+  const handleInputEnter = (e) => {
+    if (e.code === "Enter") {
       joinRoom();
     }
-  }
+  };
   return (
     <div className="homePageWrapper">
       <div className="formWrapper">
@@ -47,15 +48,54 @@ const Home = () => {
             onChange={(e) => setRoomId(e.target.value)}
             onKeyUp={handleInputEnter}
           />
-          <input 
-            type="text" 
-            placeholder="Username" 
-            className="inputbox" 
+          <input
+            type="text"
+            placeholder="Username"
+            className="inputbox"
             value={username}
-            onChange={(e)=>setUsername(e.target.value)}
+            onChange={(e) => setUsername(e.target.value)}
             onKeyUp={handleInputEnter}
           />
-          <button className="btn joinbtn" onClick={joinRoom}>Join</button>
+
+          {/* <button className="btn joinbtn" onClick={joinRoom}>Join</button> */}
+
+          <button className="joinbtn" onClick={joinRoom}>
+            Join
+            <div className="inner-button">
+              <svg
+                id="Arrow"
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+                height="30px"
+                width="30px"
+                className="icon"
+              >
+                <defs>
+                  <linearGradient
+                    y2="100%"
+                    x2="100%"
+                    y1="0%"
+                    x1="0%"
+                    id="iconGradient"
+                  >
+                    <stop
+                      style={{ stopColor: "#FFFFFF", stopOpacity: 1 }}
+                      offset="0%"
+                    />
+                    <stop
+                      style={{ stopColor: "#AAAAAA", stopOpacity: 1 }}
+                      offset="100%"
+                    />
+                  </linearGradient>
+                </defs>
+                <path
+                  fill="url(#iconGradient)"
+                  d="M4 15a1 1 0 0 0 1 1h19.586l-4.292 4.292a1 1 0 0 0 1.414 1.414l6-6a.99.99 0 0 0 .292-.702V15c0-.13-.026-.26-.078-.382a.99.99 0 0 0-.216-.324l-6-6a1 1 0 0 0-1.414 1.414L24.586 14H5a1 1 0 0 0-1 1z"
+                ></path>
+              </svg>
+            </div>
+          </button>
+
           <span className="createinfo">
             If you don't have an invite then create &nbsp;
             <a href="" className="createNewBtn" onClick={createNewRoom}>
